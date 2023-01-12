3bc265fea327dcaf44d8963a0eb294881935cdad
jiunkoo <jiunkoo0106@gmail.com>
2023-01-12 18:26:40 +0900

    add filter

--- front-end/script-main.js
+++ front-end/script-main.js
@@ -1,4 +1,4 @@
-function tabSelect(e){
+function handleTabClick(e){
     e.preventDefault();
     tabLabels.forEach(label => label.classList.remove('tab-current'));
     tabContents.forEach(content => content.style.display='none');
@@ -10,4 +10,20 @@ function tabSelect(e){
 const tabLabels=document.querySelectorAll('.tab-label');
 const tabContents=document.querySelectorAll('.tab-contents .tab-content');
 let activateContent='';
-tabLabels.forEach(label => label.addEventListener('click', tabSelect));
+tabLabels.forEach(label => label.addEventListener('click', handleTabClick));
+
+function handleDropDownClick(e) {
+  e.preventDefault();
+  if(this.classList.contains('trigger-enter')){
+    this.classList.remove('trigger-enter', 'trigger-enter-active')
+  } else{
+    filterList.forEach(filter =>
+      filter.classList.remove('trigger-enter', 'trigger-enter-active')
+    );
+    this.classList.add('trigger-enter');
+    this.classList.add('trigger-enter-active');  
+  }
+}
+
+const filterList = document.querySelectorAll('.content-filter > li');
+filterList.forEach(filter => filter.addEventListener('click', handleDropDownClick));
